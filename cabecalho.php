6b6d97d1ab7664817b277188a4abe0bc56311da1
MatheusMiliorini <miliorinimatheus@gmail.com>
2018-04-04 19:10:09 -0300

    Commit inicial

---
+++ cabecalho.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Minha Loja</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/loja.css" />
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">North Wind</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="principal">
---
+++ conecta.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "cloud.matheusmiliorini.com.br";
+    $user = "northwind";
+    $pass = "essaeminhasenha";
+    $db = "northwind";
+
+    $conexao = mysqli_connect($host,$user,$pass,$db);
+?>
